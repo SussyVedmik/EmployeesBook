8869c446ee23d1bef33f4ad4ea69d4a8d3ea930e
Alex <Chikenamigo25@gmail.com>
2023-09-07 16:46:16 +0300

    создание структуры сотрудника

---
+++ EmployeesBook/Source.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+using namespace std;
+
+struct employee {
+    char firstName[50];
+    char lastName[50];
+    char phoneNumber[20];
+    double salary;
+};
+
